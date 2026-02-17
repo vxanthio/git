eba9eab9bcbf67afde138080aca767d7b4a4c90c
vxanthio <basiliki.martios1@gmail.com>
2026-02-17 15:20:26 +0200

    feat(main): add interactive name prompt to hello.sh

--- lib/hello.sh
+++ lib/hello.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Default is "World"
-# Author: Jim Weirich <jim@example.com>
-name=${1:-"World"}
-echo "Hello, $name"
+
+echo "What's your name"
+read my_name
+
+echo "Hello, $my_name"
